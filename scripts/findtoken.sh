7cc1b3c38383f828e731f0a1c25a9faafd704f11
xulq <xiaoqiang.xlq@gmail.com>
2012-07-11 16:39:24 +0800

    add description in findtoken.sh

--- scripts/findtoken.sh
+++ scripts/findtoken.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#Find totp token sn from give tnk file by current password
+#Usage:./findtoken.sh <filename.tnk> <password>
 for i in `awk -F' ' '{print $2}'  $1 `;do
  /usr/lib/shterm/authex/jiansh-etotp-verify $i -2 9 $2|grep "verify ok"&&grep $i $1
 done
